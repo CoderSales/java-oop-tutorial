2a6385a81596bc6833835cb6c850948fccfdb4db
user7529 <32943259+CoderSales@users.noreply.github.com>
2024-03-25 04:06:46 +0000

    Add BicycleModified
    use
    increment
    and
    decrement
    abbreviated
    operator

---
+++ BicycleModified.java
@@ -0,0 +1,33 @@
+class BicycleModified {
+
+    int cadence = 0;
+    int speed = 0;
+    int gear = 1;
+
+    void changeCadence(int newValue) {
+         cadence = newValue;
+    }
+
+    void changeGear(int newValue) {
+         gear = newValue;
+    }
+
+    void speedUp(int increment) {
+         speed += increment;   
+    }
+
+    void applyBrakes(int decrement) {
+         speed -= decrement;
+    }
+
+    void printStates() {
+         System.out.println("cadence:" +
+             cadence + " speed:" + 
+             speed + " gear:" + gear);
+    }
+
+    public static void main(String[] args) {
+        BicycleModified bicycle1 = new BicycleModified();
+        bicycle1.printStates();
+    }
+}
